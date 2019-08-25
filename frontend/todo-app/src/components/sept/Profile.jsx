fa7590c83d7e98c9d7f386d261c2518f11c7d9ae
Eyup Keskin <s3686150@student.rmit.edu.au>
2019-08-25 20:11:19 +1000

    Finished initial touch ups for profile

--- frontend/todo-app/src/components/sept/Profile.jsx
+++ frontend/todo-app/src/components/sept/Profile.jsx
@@ -8,26 +8,33 @@ class Profile extends Component {
         this.state = {
             name: '',
             sid: 0,
-            course: ''
+            course: '',
+            bio: ''
         };
     }
 
 
     componentDidMount() {
-        // Create dummy profile:
+        // Create dummy profile: (comment out after testing)
         this.setState({
-            name: 'Eyup',
+            name: 'Eyup Keskin',
             sid: 3686150,
-            course: 'SoftEng'
+            course: 'SoftEng',
+            bio: 'I am very boring'
         })
     }
 
     render() {
         return (
             <div className="Profile">
-                <div>{this.state.name}</div>
-                <div>s{this.state.sid}</div>
-                <div>{this.state.course}</div>
+                <div className='proffield' id='profname'>{this.state.name}</div>
+                <div className='proffield' id='profsid'>s{this.state.sid}</div>
+                <div>
+                    <span className='proffield' id='profcourselabel'>Course:</span>
+                    <span className='proffield' id='profcourse'>{this.state.course}</span>
+                </div>
+                <div className='proffield' id='profbio'>{this.state.bio}</div>
+                <br/>
             </div>
         )
     }
