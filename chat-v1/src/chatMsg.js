5c923a2e26334b2ad908c7451db645ad515e2497
Eyup Keskin <s3686150@student.rmit.edu.au>
2019-10-01 00:15:40 +1000

    removed chat-v1 folder

--- chat-v1/src/chatMsg.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default ({name, message}) =>
-
-//Username and message text holder
-<p className="message-list">
-    <strong>{name + ":"}</strong>  <em>{message}</em>
-   
-</p>
