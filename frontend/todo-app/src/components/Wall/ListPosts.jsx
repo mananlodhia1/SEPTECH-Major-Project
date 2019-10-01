ba0e092c428fccebc6f81f1997b4d1bd7bc974dd
Shanuk Rubera <shanukrubera@gmail.com>
2019-10-02 04:48:24 +1000

    Redisigned posting system, Polished User Posts

--- frontend/todo-app/src/components/Wall/ListPosts.jsx
+++ frontend/todo-app/src/components/Wall/ListPosts.jsx
@@ -4,6 +4,7 @@ import AuthenticationService from '../todo/AuthenticationService'
 import moment from 'moment'
 import './WallStyles.css'
 import UserWall from './UserWall.jsx';
+import AppMenu from '../sept/AppMenu';
 
 class ListPost extends Component {
     constructor(props) {
@@ -80,7 +81,7 @@ class ListPost extends Component {
 
     }
 
-    createNewPost(){
+    createNewPost() {
         let username = AuthenticationService.getLoggedInUserName()
 
         let post = {
@@ -90,73 +91,48 @@ class ListPost extends Component {
         }
 
         PostService.createPost(username, post)
-                .then(() => this.refreshPosts())
+            .then(() => this.refreshPosts())
     }
 
     render() {
         let links = [
             { label: 'Home', link: '/index', },
             { label: 'Chat', link: '/index', },
-            { label: 'User Posts', link: '/posts', active: true  },
+            { label: 'User Posts', link: '/posts', active: true },
             { label: 'Profile', link: '/profile', },
             { label: 'Logout', link: '/logout' },
         ];
         console.log('render')
         return (
-            // <div>
-            //     <h1>User Posts</h1>
-            //     {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
-            //     <div className="container">
-            //         <table className="table">
-            //             <thead>
-            //                 <tr>
-            //                     <th>Post</th>
-            //                     <th>Post Date</th>
-            //                     <th>Modify</th>
-            //                     <th>Delete</th>
-            //                 </tr>
-            //             </thead>
-            //             <tbody>
-            //                 {
-            //                     this.state.posts.map(
-            //                         post =>
-            //                             <tr key={post.id}>
-            //                                 <td>{post.description}</td>
-            //                                 <td>{moment(post.targetDate).format('YYYY-MM-DD')}</td>
-            //                                 <td><button className="btn btn-success" onClick={() => this.updatePostClicked(post.id)}>Update</button></td>
-            //                                 <td><button className="btn btn-warning" onClick={() => this.deletePostClicked(post.id)}>Delete</button></td>
-            //                             </tr>
-            //                     )
-            //                 }
-            //             </tbody>
-            //         </table>
-            //         <div className="row">
-            //             <button className="btn btn-success" onClick={this.addPostClicked}>New Post</button>
-            //         </div>
-            //     </div>
-            // </div>
             <div>
-                {/* <div className="row">
-                         <button className="btn-new-post" onClick={this.addPostClicked}>New Post</button>
-                </div> */}
-                <div>
-                    <input type="text" name="description" id="descri"/>
-                    <button onClick={() => this.createNewPost()}>Post now</button>
+                <div className="container center">
+                    <AppMenu links={links} history={this.props.history} />
                 </div>
-                {
-                    this.state.posts.map(
-                        post =>
-                            <div className="post-container" key={post.id}>
-                                <p>{post.description}</p>
-                                <div className="date">{moment(post.targetDate).format('YYYY-MM-DD')}</div>
-                                <button className="btn-update" onClick={() => this.updatePostClicked(post.id)}>Update</button>
-                                <button className="btn-delete" onClick={() => this.deletePostClicked(post.id)}>Delete</button>
-                            </div>
-                    )
-                }
-                
+                <div>
+                    <div className="container center">
+                        <div className="status-update-input-box" align="center">
+                            <textarea type="text" rows="5" placeholder="What's on your mind?" name="description" id="descri" />
+                            <button className="post-button" onClick={() => this.createNewPost()}>Post now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="container center">
+                    <div className="post-container-container">
+                        {
+                            this.state.posts.map(
+                                post =>
+                                    <div className="post-container" key={post.id}>
+                                        <p>{post.description}</p>
+                                        <div className="date">{moment(post.targetDate).format('YYYY-MM-DD')}</div>
+                                        <button className="btn-update" onClick={() => this.updatePostClicked(post.id)}>Update</button>
+                                        <button className="btn-delete" onClick={() => this.deletePostClicked(post.id)}>Delete</button>
+                                    </div>
+                            )
+                        }
+                    </div>
+                </div>
             </div>
-            
+
         )
     }
 }
