cd87bb5b586dcb98846bee3541540f05bf157bea
Eyup Keskin <s3686150@student.rmit.edu.au>
2019-10-02 20:40:03 +1000

    added login class

---
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/login/Login.java
@@ -0,0 +1,44 @@
+package com.sept.rest.webservices.restfulwebservices.login;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Login {
+    @Id
+    long sid;
+    String password;
+
+    public Login(long sid, String password) {
+        this.sid = sid;
+        this.password = password;
+    }
+
+    /**
+     * @return the sid
+     */
+    public long getSid() {
+        return sid;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(long sid) {
+        this.sid = sid;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
