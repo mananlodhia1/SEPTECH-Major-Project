d3fdab0485040d95a19e1481b653c314b8f75284
Eyup Keskin <s3686150@student.rmit.edu.au>
2019-10-20 16:02:15 +1100

    added jpa repo and register function for login, not yet incorporated to jwtinmemoryuserdetails however

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/login/LoginJpaRepository.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/login/LoginJpaRepository.java
@@ -1,9 +1,13 @@
 package com.sept.rest.webservices.restfulwebservices.login;
 
+import java.util.List;
+
+import com.sept.rest.webservices.restfulwebservices.todo.Todo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginJpaRepository extends JpaRepository<Login, Long>{
-    
+    List<Login> findBySid(long sid);
 }
---
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/login/LoginJpaResource.java
@@ -0,0 +1,23 @@
+package com.sept.rest.webservices.restfulwebservices.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class LoginJpaResource {
+
+    @Autowired
+    LoginJpaRepository loginJpaRepository;
+
+    @PostMapping("jpa/users/register")
+    public ResponseEntity<Void> register(@RequestBody Login login) {
+        loginJpaRepository.save(login);
+
+        return ResponseEntity.noContent().build();
+    }
+}
